9c1d8bd2ac0953cbec5b18d454618811aee79b6f
Baher Zakhary <baher.zakharyy@gmail.com>
2022-05-28 13:58:32 +0200

    - Retain background on refresh - Init results screen - Navigate to results screen on submit - Code refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
+import { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css';
+import ResultsScreen from './screens/ResultsScreen/ResultsScreen';
 import StartScreen from './screens/StartScreen/StartScreen';
+import BusinessUtils from "./utils/BusinessUtils";
 
 function App() {
+
+  useEffect(() => {
+    const raceType = localStorage.getItem('raceType');
+    if (raceType) {
+      BusinessUtils.setRaceTypeBackground(raceType);
+    }
+  });
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Navigate to="/start" replace={true} />} exact />
         <Route path="/start" element={<StartScreen />} />
+        <Route path="/results/:raceType" element={<ResultsScreen />} />
       </Routes>
     </div>
   );
